8d83af745ff606719e5adc9fb6a26f06b9c24b7a
James Blair <james.m.blair09@gmail.com>
2020-10-28 09:00:27 -0600

    Change prediction type to prob

--- R/plumber.R
+++ R/plumber.R
@@ -10,9 +10,10 @@ library(ranger)
 model <- readr::read_rds("model.rds")
 
 # Goal ----
-# predict(model, 
-#         new_data = jsonlite::read_json("penguins.json", 
-#                                        simplifyVector = TRUE))
+# predict(model,
+#         new_data = jsonlite::read_json("penguins.json",
+#                                        simplifyVector = TRUE),
+#         type = "prob")
 
 #* @apiTitle Penguin Predictions
 
@@ -29,7 +30,7 @@ function() {
 #* @post /predict
 function(req, res) {
   # req$body is the parsed input
-  predict(model, new_data = as.data.frame(req$body))
+  predict(model, new_data = as.data.frame(req$body), type = "prob")
 }
 
 # Update UI
