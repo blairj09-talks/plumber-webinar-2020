536407f22ca5653c5e577ed0a9e191a3804d4855
James Blair <james.m.blair09@gmail.com>
2020-10-28 11:49:47 -0600

    Update to json serialization

--- R/plumber.R
+++ R/plumber.R
@@ -2,41 +2,35 @@
 # For API
 library(plumber)
 library(rapidoc)
-# For model predictions
+
+# For model
 library(parsnip)
 library(ranger)
 
-# Load model ----
+# Goal ----
 model <- readr::read_rds("model.rds")
 
-# Goal ----
-# predict(model,
-#         new_data = jsonlite::read_json("penguins.json",
-#                                        simplifyVector = TRUE),
-#         type = "prob")
+# predict(model, new_data = jsonlite::read_json("penguins.json", simplifyVector = TRUE), type = "prob")
 
-#* @apiTitle Penguin Predictions
-
-#* Determine if the API is running and listening as expected
+#* Health Check - Is the API running
 #* @get /health-check
-function() {
-  list(status = "All Good",
-       time = Sys.time())
+status <- function() {
+  list(
+    status = "All Good",
+    time = Sys.time()
+  )
 }
 
-#* Predict penguin species based on input data
-#* @parser json
-#* @serializer csv
+#* Predict species for new penguins
+#* @serializer json
 #* @post /predict
 function(req, res) {
-  # req$body is the parsed input
   predict(model, new_data = as.data.frame(req$body), type = "prob")
 }
 
-# Update UI
 #* @plumber
 function(pr) {
   pr %>% 
-    pr_set_api_spec(yaml::read_yaml("openapi.yaml")) %>%
+    pr_set_api_spec(yaml::read_yaml("openapi.yaml")) %>% 
     pr_set_docs(docs = "rapidoc")
 }
